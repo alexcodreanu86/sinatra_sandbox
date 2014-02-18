c0102be02cc86f6b85da5741a02400cadb3b2405
Apprentice <apprentice@dbc-chicago-029-34.local>
2014-02-18 14:52:44 -0600

    tested some new forms

--- sinatra_skeleton/app/controllers/index.rb
+++ sinatra_skeleton/app/controllers/index.rb
@@ -4,6 +4,11 @@ get '/' do
   erb :index
 end
 
+post '/' do
+  @some_text = params["some_text"]
+  erb :post_index
+end
+
 get '/cool_url' do
   puts "[LOG] Getting /cool_url"
   puts "[LOG] Params: #{params.inspect}"
@@ -11,7 +16,19 @@ get '/cool_url' do
 end
 
 post '/cool_url' do
+  binding.pry
   puts "[LOG] Posting to /cool_url"
   puts "[LOG] Params: #{params.inspect}"
   erb :post_cool_url
 end
+# get '/?grandma=hey' do
+#   "yolo"
+# end
+
+get "/?text = hey" do
+  @text = params["text"]
+  puts @text
+  erb :post_cool_url
+end
+
+
